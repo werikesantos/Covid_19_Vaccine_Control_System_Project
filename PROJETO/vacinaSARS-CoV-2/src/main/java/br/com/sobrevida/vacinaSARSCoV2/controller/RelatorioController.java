a045d79d3dc16dad89df17d7e123288a85a13f85
werikesantos <werike.ms@gmail.com>
2021-06-13 06:01:46 -0300

    finalizado a tela de gerar relatorio

--- PROJETO/vacinaSARS-CoV-2/src/main/java/br/com/sobrevida/vacinaSARSCoV2/controller/RelatorioController.java
+++ PROJETO/vacinaSARS-CoV-2/src/main/java/br/com/sobrevida/vacinaSARSCoV2/controller/RelatorioController.java
@@ -6,15 +6,21 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import java.awt.Desktop;
 import java.io.File;
+import java.sql.Date;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Random;
+import javax.swing.JFormattedTextField;
 
 /**
  *
@@ -24,8 +30,15 @@ public class RelatorioController{
     
     RelatorioDao relatorioDao = new RelatorioDao();
     
-    public void teste() throws IOException{
+    public void gerarPDF(JFormattedTextField de, JFormattedTextField para) throws IOException, SQLException, ParseException{
         
+        String dePegar = de.getText(); 
+        String paraPegar = para.getText();
+        
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        Date deData = new Date(format.parse(dePegar).getTime());
+        Date paraData = new Date(format.parse(paraPegar).getTime());
+
         Document relatorio = new Document();
         Random random = new Random();
         
@@ -43,7 +56,14 @@ public class RelatorioController{
             PdfWriter.getInstance(relatorio, new FileOutputStream("C:\\Users\\WERIKE\\Desktop\\RELATÓRIO DE CONTROLE DE APLICAÇÕES\\"+arquivoPDF));
 
             relatorio.open();
-
+            
+            Image logoTipo1 = Image.getInstance("src/main/resources/image/relatorioBranco.png");
+            relatorio.add(logoTipo1);
+            
+            Paragraph paragrafo0 = new Paragraph(" ");
+            paragrafo0.setAlignment(0);
+            relatorio.add(paragrafo0);
+            
             Paragraph paragrafo = new Paragraph("RELATÓRIO DE CONTROLE DE APLICAÇÕES");
             paragrafo.setAlignment(1);
             relatorio.add(paragrafo);
@@ -53,11 +73,11 @@ public class RelatorioController{
             
             PdfPTable tabela = new PdfPTable(5);
 
-            PdfPCell coluna1 = new PdfPCell(new Paragraph("Código"));
-            PdfPCell coluna2 = new PdfPCell(new Paragraph("Nome"));
-            PdfPCell coluna3 = new PdfPCell(new Paragraph("E-Mail"));
-            PdfPCell coluna4 = new PdfPCell(new Paragraph("Celular"));
-            PdfPCell coluna5 = new PdfPCell(new Paragraph("Data segunda dose"));
+            PdfPCell coluna1 = new PdfPCell(new Paragraph("CÓDIGO"));
+            PdfPCell coluna2 = new PdfPCell(new Paragraph("NOME"));
+            PdfPCell coluna3 = new PdfPCell(new Paragraph("E-MAIL"));
+            PdfPCell coluna4 = new PdfPCell(new Paragraph("CELULAR"));
+            PdfPCell coluna5 = new PdfPCell(new Paragraph("PREVISÃO"));
            
             tabela.addCell(coluna1);
             tabela.addCell(coluna2);
@@ -65,14 +85,14 @@ public class RelatorioController{
             tabela.addCell(coluna4);
             tabela.addCell(coluna5);
            
-            List<CidadaoModel> cidadao = relatorioDao.gerarPDF();
+            List<CidadaoModel> cidadao = relatorioDao.gerarPDF(deData, paraData);
             
             for(CidadaoModel cidadaoModel : cidadao){
-                coluna1 = new PdfPCell(new Paragraph(cidadaoModel.getIdCidadao()+""));
+                coluna1 = new PdfPCell(new Paragraph(cidadaoModel.getId()+""));
                 coluna2 = new PdfPCell(new Paragraph(cidadaoModel.getNome()));
                 coluna3 = new PdfPCell(new Paragraph(cidadaoModel.getEmail()));
                 coluna4 = new PdfPCell(new Paragraph(cidadaoModel.getCelular()));
-                //coluna5 = new PdfPCell(new Paragraph(cidadaoModel.getSegunda()));
+                coluna5 = new PdfPCell(new Paragraph(cidadaoModel.getPrevisao()));
 
                 tabela.addCell(coluna1);
                 tabela.addCell(coluna2);
@@ -81,7 +101,7 @@ public class RelatorioController{
                 tabela.addCell(coluna5);
             }
             
-            relatorio.add(tabela);
+            relatorio.add(tabela);   
             relatorio.close();
             Desktop.getDesktop().open(new File("C:\\Users\\WERIKE\\Desktop\\RELATÓRIO DE CONTROLE DE APLICAÇÕES\\"+arquivoPDF));
 
--- PROJETO/vacinaSARS-CoV-2/src/main/java/br/com/sobrevida/vacinaSARSCoV2/model/dao/RelatorioDao.java
+++ PROJETO/vacinaSARS-CoV-2/src/main/java/br/com/sobrevida/vacinaSARSCoV2/model/dao/RelatorioDao.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import java.sql.Date;
 
 /**
  *
@@ -17,20 +20,23 @@ public class RelatorioDao{
     PreparedStatement ps; 
     ResultSet result;
             
-    public List<CidadaoModel> gerarPDF(){
-
+    public List<CidadaoModel> gerarPDF(Date deData, Date paraData){
+        
         List<CidadaoModel> relatorioAplicacao = new ArrayList<>();
                 
         String sql = 
-            "SELECT ap.idCidadao, ci.nome, ci.email, ci.celular, ap.segunda "
+            "SELECT ap.id, ci.nome, ci.email, ci.celular, ap.data_aplicacao, ap.previsao "
             +"FROM aplicacao ap "
             +"INNER JOIN cidadao ci "
             +"ON ap.idCidadao = ci.id "
-            +"WHERE ap.segunda != 'null' ORDER BY nome ASC";
+            +"WHERE ap.previsao BETWEEN ? AND ?";
         
         try(Connection conn = connectionFactory.connection()){
             
             ps = conn.prepareStatement(sql);
+            
+            ps.setDate(1, deData);
+            ps.setDate(2, paraData);
             
             result = ps.executeQuery();
             
@@ -38,11 +44,17 @@ public class RelatorioDao{
                 
                 CidadaoModel cidadaoModel = new CidadaoModel();
                 
-                cidadaoModel.setIdCidadao(result.getInt("idcidadao"));
-                cidadaoModel.setNome(result.getString("nome"));
-                cidadaoModel.setEmail(result.getString("email"));
-                cidadaoModel.setCelular(result.getString("celular"));
-                //cidadaoModel.setSegunda(result.getString("segunda"));
+                cidadaoModel.setId(result.getInt("ap.id"));
+                cidadaoModel.setNome(result.getString("ci.nome"));
+                cidadaoModel.setEmail(result.getString("ci.email"));
+                cidadaoModel.setCelular(result.getString("ci.celular"));
+
+                String previsaoFormatada;
+                Date previsao = result.getDate("ap.previsao");
+                DateFormat dateFormat1 = new SimpleDateFormat("dd-MM-yyyy");
+                previsaoFormatada = dateFormat1.format(previsao);
+                
+                cidadaoModel.setPrevisao(previsaoFormatada);
                 
                 relatorioAplicacao.add(cidadaoModel);
             }
